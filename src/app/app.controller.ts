5cc1c5f37d60e977f7c8299f609abeb8588ccf3e
Loïc Vanden Bossche <l.vandenbossche.jouvet@gmail.com>
2022-05-20 21:59:51 +0200

    Fixed health not public

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import {Public} from "../shared/decorators/public.decorator";
 
 @Controller()
 export class AppController {
+
+  @Public()
   @Get('health')
   getHello(): string {
     return 'Exemple API is up and running!';
